39915ca786c08d869b1f61d550ccc41789a41115
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-02 12:36:30 +0300

    Выполнено первое задание. Создано меню навигации в Main.

--- BMO.GameDevUnity.CSharp1.Pract2/BMO.GameDevUnity.CSharp1.Pract2/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract2/BMO.GameDevUnity.CSharp1.Pract2/Program.cs
@@ -10,6 +10,45 @@ namespace BMO.GameDevUnity.CSharp1.Pract2
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Введите номер задания(введите exit для выхода): ");
+                string task = Console.ReadLine();
+                switch (task)
+                {
+                    case "1":
+                        Console.Write("Введите первое число: ");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Write("Введите третье число: ");
+                        double c = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Минимальным является число {0}", MinNum(a, b, c));
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        Console.WriteLine("Введены некорректные данные");
+                        break;
+                }
+            }
+            
+        }
+
+        static double MinNum(double a, double b, double c)
+        {
+            if (a<b && a<c)
+            {
+                return a;
+            }
+            else if (b<c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
         }
     }
 }
