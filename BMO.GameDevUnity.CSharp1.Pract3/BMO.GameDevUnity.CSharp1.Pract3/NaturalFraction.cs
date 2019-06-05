7d626846fde2b9082273de8d555387918031939e
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-05 21:02:39 +0300

    Создан класс натуральной дроби

---
+++ BMO.GameDevUnity.CSharp1.Pract3/BMO.GameDevUnity.CSharp1.Pract3/NaturalFraction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMO.GameDevUnity.CSharp1.Pract3
+{
+    class NaturalFraction
+    {
+    }
+}
