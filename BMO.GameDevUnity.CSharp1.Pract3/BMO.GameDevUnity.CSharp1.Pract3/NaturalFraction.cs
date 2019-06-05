f9a099f34888f53b8f52578e58911f376cffd5cb
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-05 21:56:48 +0300

    Описан класс натуральных дробей. Реализованы методы сложения, вычитания, умножения и деления.

--- BMO.GameDevUnity.CSharp1.Pract3/BMO.GameDevUnity.CSharp1.Pract3/NaturalFraction.cs
+++ BMO.GameDevUnity.CSharp1.Pract3/BMO.GameDevUnity.CSharp1.Pract3/NaturalFraction.cs
@@ -8,5 +8,78 @@ namespace BMO.GameDevUnity.CSharp1.Pract3
 {
     class NaturalFraction
     {
+        private long numerator;
+        private long denominator;
+
+        public NaturalFraction(long numerator, long denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public long GetNumerator()
+        {
+            return numerator;
+        }
+
+        public long GetDenominator()
+        {
+            return denominator;
+        }
+
+        public static NaturalFraction Sum(NaturalFraction a, NaturalFraction b)
+        {
+            long NOK = FindNOK(a.denominator, b.denominator);
+            long aNum = a.numerator;
+            long bNum = b.numerator;
+            aNum *= (NOK / a.denominator);
+            bNum *= (NOK / b.denominator);
+            return new NaturalFraction(aNum + bNum, NOK);
+        }
+
+        public static NaturalFraction Difference(NaturalFraction a, NaturalFraction b)
+        {
+            long NOK = FindNOK(a.denominator, b.denominator);
+            long aNum = a.numerator;
+            long bNum = b.numerator;
+            aNum *= (NOK / a.denominator);
+            bNum *= (NOK / b.denominator);
+            return new NaturalFraction(aNum - bNum, NOK);
+        }
+
+        public static NaturalFraction Multiplication(NaturalFraction a, NaturalFraction b)
+        {            
+            return new NaturalFraction(a.numerator * b.numerator, a.denominator * b.denominator);
+        }
+
+        public static NaturalFraction Division(NaturalFraction a, NaturalFraction b)
+        {
+            return new NaturalFraction(a.numerator * b.denominator, a.denominator * b.numerator);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Значение дроби равно = {numerator} / {denominator}");
+        }
+
+        private static long FindNOD(long a, long b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            if (a > b)
+            {
+                long tmp = a;
+                a = b;
+                b = tmp;
+            }
+            return FindNOD(a, b - a);
+        }
+
+        private static long FindNOK(long a, long b)
+        {
+            return ((a * b) / FindNOD(a, b));
+        }
     }
 }
--- BMO.GameDevUnity.CSharp1.Pract3/BMO.GameDevUnity.CSharp1.Pract3/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract3/BMO.GameDevUnity.CSharp1.Pract3/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/* *Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. 
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+ * Написать программу, демонстрирующую все разработанные элементы класса.
+* Добавить свойства типа int для доступа к числителю и знаменателю;
+* Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;
+** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException* 
+* ("Знаменатель не может быть равен 0");
+*** Добавить упрощение дробей. */
+
 namespace BMO.GameDevUnity.CSharp1.Pract3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            NaturalFraction naturalFraction1 = new NaturalFraction(1, 3);
+            NaturalFraction naturalFraction2 = new NaturalFraction(2, 5);
+
+            NaturalFraction naturalFraction3 = NaturalFraction.Sum(naturalFraction1, naturalFraction2);
+            naturalFraction3.Print();
+
+            naturalFraction3 = NaturalFraction.Difference(naturalFraction1, naturalFraction2);
+            naturalFraction3.Print();
+
+            naturalFraction3 = NaturalFraction.Multiplication(naturalFraction1, naturalFraction2);
+            naturalFraction3.Print();
+
+            naturalFraction3 = NaturalFraction.Division(naturalFraction1, naturalFraction2);
+            naturalFraction3.Print();
+            Pause();
+        }
+
+        static void Pause()
+        {
+            Console.ReadKey();
         }
     }
 }
