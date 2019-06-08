7cce747b180ed9aa0852731d72726e3c8d19ae13
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-08 21:17:10 +0300

    Создан проект

---
+++ BMO.GameDevUnity.CSharp1.Pract4/BMO.GameDevUnity.CSharp1.Pract4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMO.GameDevUnity.CSharp1.Pract4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
