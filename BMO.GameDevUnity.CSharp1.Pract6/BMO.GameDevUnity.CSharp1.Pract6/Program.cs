deacd9af53bfed70625a31db8da10c536dfc6e8f
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-17 11:43:35 +0300

    Изменение программы нахождения минимума функции для работы с делегатами

--- BMO.GameDevUnity.CSharp1.Pract6/BMO.GameDevUnity.CSharp1.Pract6/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract6/BMO.GameDevUnity.CSharp1.Pract6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace BMO.GameDevUnity.CSharp1.Pract6
 {
     public delegate double Fun(double a, double x);
+    public delegate double FunMin(double x);
 
     class Program
     {
@@ -31,8 +33,47 @@ namespace BMO.GameDevUnity.CSharp1.Pract6
             return a * Math.Sin(x);
         }
 
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static void SaveFunc(string fileName, FunMin fun, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(fun(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
         static void Main(string[] args)
         {
+            #region Первое задание
+            Console.WriteLine("Первое задание: ");
+
             Console.WriteLine("Таблица функции a*x^2:");
 
             Table(new Fun(FuncFirst), -2, 2, 10);
@@ -40,6 +81,13 @@ namespace BMO.GameDevUnity.CSharp1.Pract6
             Console.WriteLine("Таблица функции a*sin(x):");
 
             Table(new Fun(FuncSecond), -2, 2, 10);
+            #endregion
+
+            #region Второе задание
+            Console.WriteLine("Минимум функции (x * x - 50 * x + 10) на промежутке [-100, 100]:");
+            SaveFunc("data.bin", F, -100, 100, 0.5);
+            Console.WriteLine(Load("data.bin"));
+            #endregion
 
             Console.ReadKey();
         }
