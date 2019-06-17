f7d35af30df6b633f14757906fb2a03eb9374d9e
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-17 11:31:45 +0300

    Первое задание

--- BMO.GameDevUnity.CSharp1.Pract6/BMO.GameDevUnity.CSharp1.Pract6/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract6/BMO.GameDevUnity.CSharp1.Pract6/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace BMO.GameDevUnity.CSharp1.Pract6
 {
+    public delegate double Fun(double a, double x);
+
     class Program
     {
+        public static void Table(Fun F, double a, double x, double b)
+        {
+            Console.WriteLine("----- a ----------- x ----- F(a, x)-");
+            for (int i = 0; i < b; i++)
+            {
+                Console.WriteLine("| {0,8:0.000} | | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+                a++; x++;
+            }
+            Console.WriteLine("------------------------------------");
+        }
+
+        public static double FuncFirst(double a, double x)
+        {
+            return a*Math.Pow(x, 2);
+        }
+
+        public static double FuncSecond(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Таблица функции a*x^2:");
+
+            Table(new Fun(FuncFirst), -2, 2, 10);
+
+            Console.WriteLine("Таблица функции a*sin(x):");
+
+            Table(new Fun(FuncSecond), -2, 2, 10);
+
+            Console.ReadKey();
         }
     }
 }
