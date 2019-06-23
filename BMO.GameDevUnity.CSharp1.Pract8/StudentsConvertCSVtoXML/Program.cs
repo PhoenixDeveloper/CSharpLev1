af76df5224ba7e985e22927c2b12fee3d0646aec
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-23 16:44:25 +0300

    Пятое задание выполнено

--- BMO.GameDevUnity.CSharp1.Pract8/StudentsConvertCSVtoXML/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract8/StudentsConvertCSVtoXML/Program.cs
@@ -16,7 +16,7 @@ namespace StudentsConvertCSVtoXML
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormConvert());
         }
     }
 }
