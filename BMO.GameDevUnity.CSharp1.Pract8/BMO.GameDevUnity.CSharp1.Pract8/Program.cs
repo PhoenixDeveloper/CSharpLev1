0bb8f58388bba552521e6c534d74b978e59c1b1b
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-23 13:32:41 +0300

    Выполнено второе задание

--- BMO.GameDevUnity.CSharp1.Pract8/BMO.GameDevUnity.CSharp1.Pract8/Program.cs
+++ BMO.GameDevUnity.CSharp1.Pract8/BMO.GameDevUnity.CSharp1.Pract8/Program.cs
@@ -16,7 +16,7 @@ namespace BMO.GameDevUnity.CSharp1.Pract8
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
